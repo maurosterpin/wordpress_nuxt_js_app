ebac4d8741e4b432dae2b4ff613a3d5ea2dbc820
MSterpin <92162018+maurosterpin@users.noreply.github.com>
2022-03-18 13:40:50 +0100

    nuxt js

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+  computed: {
+    posts() {
+      return []
+    },
+  },
+})
+</script>
+
+<style scoped></style>
