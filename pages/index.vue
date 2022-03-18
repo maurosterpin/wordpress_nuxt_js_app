9e12b7679f9f10a64bbc1c1892417aba51f04b14
MSterpin <92162018+maurosterpin@users.noreply.github.com>
2022-03-18 13:48:59 +0100

    template

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,21 @@
 <template>
-  <div></div>
+  <div class="container">
+    <div>
+      <h1 class="text-center">Headless WordPress Nuxt</h1>
+      <div class="container py-4">
+        <div class="row">
+          <post-card v-for="post in posts" :key="post.slug" :post="post" />
+          <div class="d-flex w-100">
+            <div class="ml-auto" v-if="pageInfo.hasNextPage">
+              <NuxtLink :to="{ query: { after: pageInfo.endCursor } }"
+                ><span>Next Page</span></NuxtLink
+              >
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -9,7 +25,22 @@ export default Vue.extend({
   name: 'IndexPage',
   computed: {
     posts() {
-      return []
+      return [
+        {
+          title: 'test post',
+          slug: 'test-post',
+          content: '<p>this is a test post</p>',
+          excerpt: '<p>this is a test post</p>',
+        },
+      ]
+    },
+    pageInfo() {
+      return {
+        endCursor: '',
+        hasNextPage: false,
+        hasPreviousPage: false,
+        startCursor: '',
+      }
     },
   },
 })
